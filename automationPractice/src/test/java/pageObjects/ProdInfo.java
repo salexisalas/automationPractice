c28dd1a3776fc942c1317dc134bc052e2fa1d309
salomonfdo <Saufe713>
2020-04-01 16:22:25 -0700

    Update ProdInfo.java

--- automationPractice/src/test/java/pageObjects/ProdInfo.java
+++ automationPractice/src/test/java/pageObjects/ProdInfo.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ProdInfo {
+	public int c;
 	
 	public ProdInfo(WebDriver driver) {
 		  PageFactory.initElements(driver, this);
