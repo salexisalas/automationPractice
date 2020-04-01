593b8706b05aa6a57e50449d6a6c3cf9ed272914
carlos <carlosadriansalasc@gmail.com>
2020-04-01 17:37:38 -0600

    HomePage, LoginPage, ShoppingCartPage
    elements of mentioned page objects

---
+++ automationPractice/src/test/java/pageObjects/LogInPage.java
@@ -0,0 +1,45 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LogInPage {
+	public LogInPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(id = "email")
+	WebElement emaillbl;
+	
+	@FindBy(id = "passwd")
+	WebElement passwdlbl;
+	
+	@FindBy(id = "SubmitLogin")
+	WebElement submitBtn;
+	
+	@FindBy(className = "icon-home")
+	WebElement homeBtn;
+	
+	@FindBy(id = "email_create")
+	WebElement emailCreatelbl;
+	
+	@FindBy(id = "SubmitCreate")
+	WebElement createBtn;
+	
+//	public void sendEmail(String email) {
+//		emaillbl.sendKeys(email);
+//	}
+//	
+//	public void sendPass(String pass) {
+//		passwdlbl.sendKeys(pass);
+//	}
+//	
+//	public void clickSubmit() {
+//		submitBtn.click();
+//	}
+
+}
+
+
