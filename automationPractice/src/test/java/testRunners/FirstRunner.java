7e5584640bfc416ddb5b1c006f7130ebecc338dc
carlos <carlosadriansalasc@gmail.com>
2020-04-01 17:17:41 -0600

    TESNG FirstRunner

--- automationPractice/src/test/java/testRunners/FirstRunner.java
+++ automationPractice/src/test/java/testRunners/FirstRunner.java
@@ -1,5 +1,16 @@
 package testRunners;
 
-public class FirstRunner {
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+		features = "src/test/resources/featureFiles",
+		glue = {"stepDefinitions"},
+		tags = {"@BuyProduct"}
+		
+		)
+public class FirstRunner extends AbstractTestNGCucumberTests {
 
 }
