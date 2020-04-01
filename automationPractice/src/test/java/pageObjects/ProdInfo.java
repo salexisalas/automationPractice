99f58c1882028a03394a1986ba14b2753893a0df
salomonfdo <Saufe713>
2020-04-01 16:26:51 -0700

    ProdInfo
    Se muestran los elementos que se encuentran al seleccionar un producto.

--- automationPractice/src/test/java/pageObjects/ProdInfo.java
+++ automationPractice/src/test/java/pageObjects/ProdInfo.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ProdInfo {
-	public int c;
 	
 	public ProdInfo(WebDriver driver) {
 		  PageFactory.initElements(driver, this);
