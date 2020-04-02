9377cc4e0580afa5fcb48dd96c468393654bbb18
salomonfdo <Saufe713>
2020-04-01 17:02:31 -0700

    SetUpClassChromeDriver
    This show the path of the driver

--- automationPractice/src/test/java/stepDefinitions/SetUpClass.java
+++ automationPractice/src/test/java/stepDefinitions/SetUpClass.java
@@ -15,6 +15,7 @@ public class SetUpClass {
 	public void setUp(){
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Academia/Documents/GitHub/automationPractice/automationPractice/src/test/resources/drivers/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", "C:/Users/test.admin/Documents/GitHub/automationPractice/automationPractice/src/test/resources/drivers/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
