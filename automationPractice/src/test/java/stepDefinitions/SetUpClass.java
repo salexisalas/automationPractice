2bfd1416f813c29a59da067caf381a5e11c32749
salomonfdo <Saufe713>
2020-04-01 17:06:33 -0700

    SetUp

--- automationPractice/src/test/java/stepDefinitions/SetUpClass.java
+++ automationPractice/src/test/java/stepDefinitions/SetUpClass.java
@@ -13,7 +13,6 @@ public class SetUpClass {
 	public static WebDriver driver;
 	@Before
 	public void setUp(){
-		System.setProperty("webdriver.chrome.driver", "C:/Users/Academia/Documents/GitHub/automationPractice/automationPractice/src/test/resources/drivers/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		
