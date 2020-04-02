08600c68f69d455872f228caab4ca49e8fb2ae2b
salomonfdo <Saufe713>
2020-04-01 17:06:02 -0700

    gitIgnore

--- automationPractice/src/test/java/stepDefinitions/SetUpClass.java
+++ automationPractice/src/test/java/stepDefinitions/SetUpClass.java
@@ -14,7 +14,6 @@ public class SetUpClass {
 	@Before
 	public void setUp(){
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Academia/Documents/GitHub/automationPractice/automationPractice/src/test/resources/drivers/chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver", "C:/Users/test.admin/Documents/GitHub/automationPractice/automationPractice/src/test/resources/drivers/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		
