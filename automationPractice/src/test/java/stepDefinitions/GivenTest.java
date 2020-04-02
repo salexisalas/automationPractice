b0c6f5bdf0239f0b66ede10fa65879921d3a9b04
salomonfdo <62582258+salomonfdo@users.noreply.github.com>
2020-04-01 21:28:30 -0700

    Given
    Update of Given file

--- automationPractice/src/test/java/stepDefinitions/GivenTest.java
+++ automationPractice/src/test/java/stepDefinitions/GivenTest.java
@@ -9,6 +9,20 @@ public class GivenTest {
 	public static WebDriver driver = SetUpClass.driver;
 	ContactUsPage cu;
 	
+	HomePage hp = new HomePage(driver);;
+	ProdInfo pi = new ProdInfo(driver);;
+
+	@Given("User clicks on Send to a page link")
+	public void user_clicks_on_Send_to_a_page_link() {
+		
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		hp.clickProduct(driver);
+		hp.productClick();
+		pi.friendemailbtn();
+	}
+	
+	
 	@Given("User is on the Home Page")
 	public void user_is_on_the_Home_Page() {
 		driver.get("http://automationpractice.com/index.php");
